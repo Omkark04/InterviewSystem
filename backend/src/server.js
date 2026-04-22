77fe3b71107608f34bdebf6f235339445ddc5757
rohansapate21 <sapaterohan14@gmail.com>
2026-04-22 23:03:10 +0530

    server js changes railway backend specific deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,21 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const __dirname = path.resolve();
 
 console.log(ENV.PORT);
 console.log(ENV.DB_URL);
 
+// added later
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up nd running" });
 });
@@ -19,19 +26,19 @@ app.get("/books", (req, res) => {
 });
 
 //make app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(ENV.PORT, () =>
+    app.listen(ENV.PORT || 5000, "0.0.0.0", () =>
       console.log("server is running on port :", ENV.PORT)
     );
   } catch (error) {
